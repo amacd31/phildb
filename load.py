286d221e3a427430a3d5b16f62cec95ff8832b04
Andrew MacDonald <andrew@maccas.net>
2013-10-16 21:23:56 +1100

    Load multiple json files.

--- load.py
+++ load.py
@@ -1,12 +1,14 @@
+import sys
+
 import datetime
 import json
 #from matplotlib import pyplot as plt
 
 from tsdb.writer import TSWriter
 
-def load():
+def load(input_file):
     #return json.load(open('./IDN60903.94926.json'))
-    return json.load(open('./data2.json'))
+    return json.load(open(input_file))
 
 
 def parse(station_json, measurand):
@@ -20,16 +22,27 @@ def parse(station_json, measurand):
 
     return dates, data
 
+def new_file():
+    with TSWriter('test.hdf5', 'w') as writer:
+        writer.create_skeleton()
 
-def write(measurand='air_temp'):
-    x = parse(load(), measurand)
+def write(x):
 
     with TSWriter('test.hdf5', 'a') as writer:
-        #writer.create_skeleton()
         for date, value in zip(x[0], x[1]):
             writer.write(date, value)
 
-write()
+new_file()
+measurand='air_temp'
+print len(sys.argv)
+for i in range(1, len(sys.argv)):
+    print "Processing file: ", sys.argv[i], '...'
+    try:
+        x = parse(load(sys.argv[i]), measurand)
+        write(x)
+    except ValueError, e:
+        print "Skipping unloadable json file: ", sys.argv[i]
+        pass
 
 
 #def plot_data(measurand='air_temp'):
