1dc626a9c2969b1388c3ec03d1b6d3d1dcec10ec
Andrew MacDonald <andrew@maccas.net>
2014-02-16 20:05:28 +1100

    Allow creation into an existing but empty dir.
    Added unit tests for create.py as well.

--- tsdb/create.py
+++ tsdb/create.py
@@ -12,6 +12,9 @@ def create(tsdb_path):
 
     if not os.path.exists(tsdb_path):
         os.makedirs(tsdb_path)
+        os.makedirs(os.path.join(tsdb_path, 'data'))
+    elif not os.listdir(tsdb_path):
+        os.makedirs(os.path.join(tsdb_path, 'data'))
     else:
         raise ValueError('TSDB already exists at: {0}'.format(tsdb_path))
 
---
+++ tsdb/test_create.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+import tempfile
+import unittest
+
+from .database import TSDB
+from .create import create
+
+class CreateDatabaseTest(unittest.TestCase):
+    def setUp(self):
+        self.test_data_dir = os.path.join(os.path.dirname(__file__), 'test_data')
+        self.temp_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        try:
+            shutil.rmtree(self.temp_dir)
+        except OSError as e:
+            if e.errno != 2: # Code 2: No such file or directory.
+                raise
+
+    def test_create_new(self):
+        db_name = os.path.join(self.temp_dir, 'new_project')
+        create(db_name)
+        db = TSDB(db_name)
+        self.assertEqual(os.path.exists(db._TSDB__meta_data_db()), True)
+
+    def test_create_existing_dir(self):
+        db_name = os.path.join(self.temp_dir)
+        create(db_name)
+        db = TSDB(db_name)
+        self.assertEqual(os.path.exists(db._TSDB__meta_data_db()), True)
+
+    def test_protect_existing(self):
+        db_name = os.path.join(self.test_data_dir, 'test_tsdb')
+        print db_name
+        with self.assertRaises(ValueError) as context:
+            create(db_name)
+
+        self.assertEqual(context.exception.message,
+            "TSDB already exists at: {0}".format(db_name))
