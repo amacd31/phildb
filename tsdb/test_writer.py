f9eef031c27f8095459e682724a19725e51af3fd
Andrew MacDonald <andrew@maccas.net>
2014-02-22 15:26:58 +1100

    Base write code for smart updates.

--- tsdb/test_writer.py
+++ tsdb/test_writer.py
@@ -6,10 +6,17 @@ import unittest
 from datetime import datetime
 
 from . import writer
+from . import reader
 
 class WriterTest(unittest.TestCase):
     def setUp(self):
         self.tsdb_path = tempfile.mkdtemp()
+        self.tsdb_existing_file = os.path.join(self.tsdb_path, 'existing_test.tsdb')
+        shutil.copy(os.path.join(os.path.dirname(__file__),
+            'test_data',
+            'sample.tsdb'),
+            self.tsdb_existing_file)
+
         self.tsdb_file = os.path.join(self.tsdb_path, 'write_test.tsdb')
 
     def tearDown(self):
@@ -25,4 +32,61 @@ class WriterTest(unittest.TestCase):
             datafile = file.read()
 
         self.assertEqual('06606801154cbfdc8e1b8c7b1e3c1956', md5.md5(datafile).hexdigest())
+
+    def test_update_single(self):
+        modified = writer.write(self.tsdb_existing_file, [[datetime(2014,1,2)], [2.5]])
+        self.assertEqual(1, len(modified))
+        self.assertEqual([(1388620800, 2.0, 0)], modified)
+
+        data = reader.read_all(self.tsdb_existing_file)
+        self.assertEqual(2.5, data.values[1][0])
+
+    def test_update_multiple(self):
+        modified = writer.write(self.tsdb_existing_file, [[datetime(2014,1,2),datetime(2014,1,3)], [2.5, 3.5]])
+        self.assertEqual(2, len(modified))
+        self.assertEqual((1388620800, 2.0, 0), modified[0])
+        self.assertEqual((1388707200, 3.0, 0), modified[1])
+
+        data = reader.read_all(self.tsdb_existing_file)
+        self.assertEqual(2.5, data.values[1][0])
+        self.assertEqual(3.5, data.values[2][0])
+
+    def test_append_single(self):
+        modified = writer.write(self.tsdb_existing_file, [[datetime(2014,1,4)], [4.0]])
+        self.assertEqual([], modified)
+
+        data = reader.read_all(self.tsdb_existing_file)
+        self.assertEqual(4.0, data.values[-1][0])
+
+    def test_append_multiple(self):
+        modified = writer.write(self.tsdb_existing_file, [[datetime(2014,1,4), datetime(2014,1,5), datetime(2014,1,6)], [4.0, 5.0, 6.0]])
+        self.assertEqual([], modified)
+
+        data = reader.read_all(self.tsdb_existing_file)
+        self.assertEqual(1.0, data.values[0][0])
+        self.assertEqual(2.0, data.values[1][0])
+        self.assertEqual(3.0, data.values[2][0])
+        self.assertEqual(4.0, data.values[3][0])
+        self.assertEqual(5.0, data.values[4][0])
+        self.assertEqual(6.0, data.values[5][0])
+
+    def test_update_and_append(self):
+        modified = writer.write(self.tsdb_existing_file, [[datetime(2014,1,2), datetime(2014,1,3), datetime(2014,1,4), datetime(2014,1,5), datetime(2014,1,6)], [2.5, 3.0, 4.0, 5.0, 6.0]])
+
+        self.assertEqual(1, len(modified))
+        self.assertEqual((1388620800, 2.0, 0), modified[0])
+
+        data = reader.read_all(self.tsdb_existing_file)
+        self.assertEqual(1.0, data.values[0][0])
+        self.assertEqual(2.5, data.values[1][0])
+        self.assertEqual(3.0, data.values[2][0])
+        self.assertEqual(4.0, data.values[3][0])
+        self.assertEqual(5.0, data.values[4][0])
+        self.assertEqual(6.0, data.values[5][0])
+        self.assertEqual(datetime(2014,1,1), data.index[0].to_pydatetime())
+        self.assertEqual(datetime(2014,1,2), data.index[1].to_pydatetime())
+        self.assertEqual(datetime(2014,1,3), data.index[2].to_pydatetime())
+        self.assertEqual(datetime(2014,1,4), data.index[3].to_pydatetime())
+        self.assertEqual(datetime(2014,1,5), data.index[4].to_pydatetime())
+        self.assertEqual(datetime(2014,1,6), data.index[5].to_pydatetime())
 
--- tsdb/writer.py
+++ tsdb/writer.py
@@ -1,6 +1,11 @@
 import calendar
+from datetime import datetime as dt
 import os
-import struct
+from struct import pack, unpack, calcsize
+
+field_names = ['date', 'value', 'metaID']
+entry_format = 'ldi' # long, double, int; See field names above.
+entry_size = calcsize(entry_format)
 
 def bulk_write(tsdb_file, x):
     """
@@ -8,20 +13,77 @@ def bulk_write(tsdb_file, x):
     """
     with open(tsdb_file, 'wb') as writer:
         for date, value in zip(x[0], x[1]):
-            data = struct.pack('ldi', calendar.timegm(date.utctimetuple()), value, 0)
+            data = pack('ldi', calendar.timegm(date.utctimetuple()), value, 0)
             writer.write(data)
 
-def write(tsdb_file, timeseries_id, x):
+def write(tsdb_file, ts):
     """
-        Good for initial bulk load. Expects continuous time series.
+        Smart write. Expects continuous time series.
+
+        Will only update existing values where they have changed.
+        Changed existing values are returned in a list.
     """
-    start_date = x[0]
-    end_date = x[-1]
+    start_date = ts[0][0]
+    end_date = ts[0][-1]
 
-    delta = start_date - end_date
-    assert delta.days == len(x)
+    assert (end_date - start_date).days + 1 == len(ts[0])
 
-    with open(tsdb_file, 'wb') as writer:
-        for date, value in zip(x[0], x[1]):
-            data = struct.pack('ldi', calendar.timegm(date.utctimetuple()), value, 0)
-            writer.write(data)
+    with open(tsdb_file, 'rb') as reader:
+        first_record = unpack(entry_format, reader.read(entry_size))
+        reader.seek(entry_size * -1, os.SEEK_END)
+        last_record = unpack(entry_format, reader.read(entry_size))
+
+    first_record_date = dt.utcfromtimestamp(first_record[0])
+    last_record_date = dt.utcfromtimestamp(last_record[0])
+    modified_entries = []
+
+    # We are updating existing data
+    if start_date <= last_record_date:
+        offset = (start_date - first_record_date).days
+
+        with open(tsdb_file, 'r+b') as writer:
+            # If we write past the end of the file
+            records = []
+            if end_date >= last_record_date:
+                # Read until the eof for comparisons
+                writer.seek(entry_size * offset, os.SEEK_SET)
+
+                for record in iter(lambda: writer.read(entry_size), ""):
+                    records.append(unpack(entry_format, record))
+            elif end_date < last_record_date and end_date >= first_record_date:
+                # Read overlapping for comparisons
+                writer.seek(entry_size * offset, os.SEEK_SET)
+
+                for record in iter(lambda: writer.read(entry_size), ""):
+                    records.append(unpack(entry_format, record))
+
+            records_length = len(records)
+
+            # Start a count for records from the starting write position
+            rec_count = 0
+            writer.seek(entry_size * offset, os.SEEK_SET)
+            for date, value in zip(ts[0], ts[1]):
+                datestamp = calendar.timegm(date.utctimetuple())
+                overlapping = rec_count <= records_length - 1
+                if overlapping and records[rec_count][1] == value:
+                    # Skip writing the entry if it hasn't changed.
+                    writer.seek(entry_size * (rec_count +1) + (entry_size * offset), os.SEEK_SET)
+                elif overlapping and records[rec_count][1] != value:
+                    modified_entries.append(records[rec_count])
+                    data = pack('ldi', datestamp, value, 0)
+                    writer.write(data)
+                else:
+                    data = pack('ldi', datestamp, value, 0)
+                    writer.write(data)
+                rec_count += 1
+
+    # We are appending data
+    elif start_date > last_record_date and (start_date - last_record_date).days == 1:
+        with open(tsdb_file, 'ab') as writer:
+            for date, value in zip(ts[0], ts[1]):
+                data = pack('ldi', calendar.timegm(date.utctimetuple()), value, 0)
+                writer.write(data)
+    else: # Not yet supported
+        raise NotImplementedError
+
+    return modified_entries
