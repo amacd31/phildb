35e0aecafeb3e6a9a5ee9d7235a2efa15148f9c1
Andrew MacDonald <andrew@maccas.net>
2014-03-19 10:16:31 +1100

    Update HRS example to set up TimeseriesInstances.
    Also loads initial metadata in the example.

--- examples/hrs/load_hrs_data.py
+++ examples/hrs/load_hrs_data.py
@@ -8,13 +8,19 @@ print "Writing to TSDB({0})".format(sys.argv[1])
 db = TSDB(sys.argv[1])
 db.add_measurand('Q', 'STREAMFLOW', 'Streamflow')
 
+hrs_header_len = 18
+
 for i in range(2, len(sys.argv)):
     print "Processing file: ", sys.argv[i], '...'
     station_id = os.path.basename(sys.argv[i]).split('_')[0]
     print "Using station ID: ", station_id, '...'
     try:
-        df = pandas.read_csv(sys.argv[i], parse_dates=True, index_col='Date', skiprows=18)
+        with open(sys.argv[i]) as datafile:
+            header=[datafile.next() for x in xrange(hrs_header_len)]
+        header = ''.join(header)
+        df = pandas.read_csv(sys.argv[i], parse_dates=True, index_col='Date', skiprows=hrs_header_len)
         db.add_timeseries(station_id)
+        db.add_timeseries_instance(station_id, 'Q', header)
         db.bulk_write(station_id, 'Q', (df.index, df['Q'].values))
     except ValueError, e:
         print "Skipping unloadable text file: ", sys.argv[i]
