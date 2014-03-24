787383e316960ed626a760ba76c491e615938496
Andrew MacDonald <andrew@maccas.net>
2014-03-24 21:34:31 +1100

    File should have been committed with 85ed2e3
    See: 85ed2e3e788e9d00a9661da79d11aae6164342e8

---
+++ examples/hrs/autocorr.py
@@ -0,0 +1,15 @@
+import sys
+
+import numpy as np
+from tsdb.database import TSDB
+
+db = TSDB(sys.argv[1])
+
+ac = np.array(
+        [ (hrs_id, db.read_all(hrs_id, 'Q', 'BOM_HRS').value.autocorr()) for hrs_id in db.list()],
+        dtype = [('name','S8'),('val',float)]
+        )
+
+
+for station in ac[ac['val'] >= 0.95]:
+    print station['name'], station['val']
