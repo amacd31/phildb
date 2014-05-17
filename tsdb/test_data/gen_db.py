8e16fa8e5f166b41928115a1664ab4122ecc700a
Andrew MacDonald <andrew@maccas.net>
2014-05-17 16:52:29 +1000

    Convenience script to build test database.
    Creates a test TSDB using existing creation methods and populates with
    some test/example data.
    
    Can be used to generate a new test TSDB when updating the schema.

---
+++ tsdb/test_data/gen_db.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+import sys
+import datetime
+import pandas
+from tsdb.database import TSDB
+from tsdb.create import create
+
+test_tsdb_path = os.path.join(os.path.dirname(__file__), 'test_tsdb')
+
+try:
+    shutil.rmtree(test_tsdb_path)
+except OSError as e:
+    if e.errno != 2: # Code 2: No such file or directory.
+        raise
+
+create(test_tsdb_path)
+db = TSDB(test_tsdb_path)
+
+db.add_measurand('Q', 'STREAMFLOW', 'Streamflow')
+db.add_source('DATA_SOURCE', '')
+
+db.add_timeseries('410730')
+db.add_timeseries_instance('410730', 'Q', 'DATA_SOURCE', '')
+db.bulk_write('410730', 'Q', [[datetime.date(2014, 1, 1),
+            datetime.date(2014, 1, 2),
+            datetime.date(2014, 1, 3)],
+            [1,2,3]],
+            'DATA_SOURCE'
+        )
