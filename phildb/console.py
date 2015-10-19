b149138af3d3cda1b7976252d922b2d001206474
Andrew MacDonald <andrew@maccas.net>
2015-10-19 16:36:04 +1100

    Update phil command to have --version option
    Updated to use argparse in the process.
    
    Signed-off-by: Andrew MacDonald <andrew@maccas.net>

--- phildb/console.py
+++ phildb/console.py
@@ -1,3 +1,4 @@
+import argparse
 from datetime import datetime, date
 import sys
 
@@ -8,16 +9,30 @@ from matplotlib import pyplot as plt
 from IPython.terminal.embed import InteractiveShellEmbed
 ipshell = InteractiveShellEmbed()
 
+from phildb import __version__
 from phildb.database import PhilDB
 
-print("")
-if len(sys.argv) != 2:
-    print("Require a PhilDB database to be specified.\n\nUsage:\n\tphil PHILDB_PATH")
-else:
-    db = PhilDB(sys.argv[1])
+def main():
+    parser = argparse.ArgumentParser(description='Open PhilDB database.')
+    parser.add_argument('dbname', help="PhilDB database to open", nargs='?')
+    parser.add_argument('--version', action='store_true', help="Print version and exit.")
+
+    args = parser.parse_args()
+
+    if args.version:
+        print("PhilDB version: {0}".format(__version__))
+        exit()
+
+    if args.dbname is None:
+        print(parser.print_help())
+        exit()
+
+    db = PhilDB(args.dbname)
     ipshell("Running timeseries database: {0}\n"
             "Access the 'db' object to operate on the database.\n"
             "\n"
             "Run db.help() for a list of available commands.".format(db)
         )
 
+if __name__ == "__main__":
+    main()
