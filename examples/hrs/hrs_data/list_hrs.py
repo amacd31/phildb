5c3e3ddca51ce15952c03a727877c4987418b3cf
Andrew MacDonald <andrew@maccas.net>
2016-09-03 20:05:12 +1000

    Fix print statement calls in HRS example scripts
    Signed-off-by: Andrew MacDonald <andrew@maccas.net>

--- examples/hrs/hrs_data/list_hrs.py
+++ examples/hrs/hrs_data/list_hrs.py
@@ -2,4 +2,4 @@ import json
 
 x = json.load(open('site_config.json'))
 
-for station in x['stations']['features']: print station['properties']['AWRC_ID']
+for station in x['stations']['features']: print(station['properties']['AWRC_ID'])
