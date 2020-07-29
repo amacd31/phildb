50836c6b2768399ea6dec0d9c7581f7d6ddfa232
Andrew MacDonald <andrew@maccas.net>
2020-07-29 16:10:45 +1000

    Drop support for EOL Pythons 2.7, 3.4, & 3.5
    3.5 isn't actually EOL for another few months, but dropping support now
    anyway for CI testing.
    
    Signed-off-by: Andrew MacDonald <andrew@maccas.net>

--- README.rst
+++ README.rst
@@ -21,8 +21,8 @@ software, and includes an evaluation between InfluxDB, PhilDB, and SciDB.
 Dependencies
 ------------
 
-Requires Python 2.7 or greater (mostly tested with Python 2.7 and Python 3.5 on Mac OSX and Linux).
-Test suite runs on Linux using Travis CI with Python 2.7, 3.4, 3.5, and 3.6.
+Requires Python 3.7 or greater (mostly tested on Mac OSX and Linux).
+Test suite runs on Linux using Travis CI with Python 3.6, 3.7, and 3.8.
 Test suite runs on Windows using Appveyor with Python 3.4.
 
 All the python dependencies are recorded in the python\_requirements
--- setup.py
+++ setup.py
@@ -45,11 +45,9 @@ setup(
         "Topic :: Software Development :: Build Tools",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
